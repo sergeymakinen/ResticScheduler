571e60514b2e042f01a50ef7f9347e2fd5ca60f8
Sergey Makinen <sergey@makinen.ru>
2025-06-08 12:50:45 +0300

    Fix PKs for Keychain passwords (fixes #8)

--- ResticScheduler/Utilities/KeychainPassword.swift
+++ ResticScheduler/Utilities/KeychainPassword.swift
@@ -15,7 +15,11 @@ extension String: DataRepresentable {
     }
 
     func data() -> Data? {
-        data(using: .utf8)
+        guard count > 0 else {
+            return nil
+        }
+
+        return data(using: .utf8)
     }
 }
 
@@ -25,7 +29,11 @@ extension String?: DataRepresentable {
     }
 
     func data() -> Data? {
-        self?.data(using: .utf8)
+        guard let self, !self.isEmpty else {
+            return nil
+        }
+
+        return self.data(using: .utf8)
     }
 }
 
@@ -33,24 +41,19 @@ protocol KeychainPasswordKey {
     associatedtype Value: DataRepresentable
 
     static var label: String { get }
-    static var account: String? { get }
+    static var account: String { get }
     static var defaultValue: Value { get }
 }
 
 extension KeychainPasswordKey {
-    static var account: String? { nil }
+    static var account: String { label }
 
-    fileprivate static var query: [CFString: Any] {
-        var query: [CFString: Any] = [
-            kSecClass: kSecClassGenericPassword,
-            kSecAttrService: Bundle.main.bundleIdentifier!,
-            kSecAttrLabel: label,
-        ]
-        if let account {
-            query[kSecAttrAccount] = account
-        }
-        return query
-    }
+    fileprivate static var query: [CFString: Any] { [
+        kSecClass: kSecClassGenericPassword,
+        kSecAttrAccount: account,
+        kSecAttrService: Bundle.main.bundleIdentifier!,
+        kSecAttrLabel: label,
+    ] }
 }
 
 struct KeychainPasswordValues {
@@ -99,26 +102,40 @@ struct KeychainPasswordValues {
                                 return
                             }
 
-                            objectWillChange.send()
                             let data = newValue.data()
+                            _value = data != nil ? newValue : key.defaultValue as! Value
+                            isRead = true
+                            notifySyncQueue.sync {
+                                self.notifyItem?.cancel()
+                                let notifyItem = DispatchWorkItem { [weak self] in
+                                    self?.notifySyncQueue.sync {
+                                        DispatchQueue.main.async { [weak self] in
+                                            self?.objectWillChange.send()
+                                        }
+                                    }
+                                }
+                                self.notifyItem = notifyItem
+                                self.notifyQueue.asyncAfter(deadline: .now() + 0.5, execute: notifyItem)
+                            }
                             let query = key.query
                             var status: OSStatus
+                            var action: String
                             if let data {
                                 let update = [kSecValueData: data]
+                                action = "add"
                                 status = SecItemAdd(query.merging(update) { _, new in new } as CFDictionary, nil)
                                 if status == errSecDuplicateItem {
+                                    action = "update"
                                     status = SecItemUpdate(query as CFDictionary, update as CFDictionary)
                                 }
                             } else {
+                                action = "delete"
                                 status = SecItemDelete(query as CFDictionary)
                             }
-                            guard status == errSecSuccess else {
-                                TypeLogger.function().error("Couldn't \(isNil(newValue) ? "delete" : "set") keychain item: \(secErrorMessage(status), privacy: .public)")
+                            guard status == errSecSuccess || (data == nil && status == errSecItemNotFound) else {
+                                TypeLogger.function().error("Couldn't \(action, privacy: .public) keychain item: \(secErrorMessage(status), privacy: .public)")
                                 return
                             }
-
-                            _value = data != nil ? newValue : key.defaultValue as! Value
-                            isRead = true
                         }
                     }
                 }
@@ -129,6 +146,9 @@ struct KeychainPasswordValues {
             private let key: any KeychainPasswordKey.Type
             private var _value: Value
             private var isRead = false
+            private var notifyItem: DispatchWorkItem?
+            private let notifyQueue = DispatchQueue(label: "\(Bundle.main.bundleIdentifier!).\(Value.self).notify", qos: .userInitiated)
+            private let notifySyncQueue = DispatchQueue(label: "\(Bundle.main.bundleIdentifier!).\(Value.self).notifySync", qos: .userInitiated)
 
             init(key: any KeychainPasswordKey.Type) {
                 self.key = key
